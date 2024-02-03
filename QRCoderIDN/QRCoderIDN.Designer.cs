47c7947a7a75830444f9023a1d9768c3b7e9fde0
idincern <idincern@gmail.com>
2024-02-03 16:00:00 +0300

    Add project files.

---
+++ QRCoderIDN/QRCoderIDN.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace QRCoderIDN
+{
+    partial class QRCoderIDN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 539);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 61);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // QRCoderIDN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 612);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "QRCoderIDN";
+            this.ShowIcon = false;
+            this.Text = "QRCoderIDN";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ QRCoderIDN/QRCoderIDN.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRCoderIDN
+{
+    public partial class QRCoderIDN : Form
+    {
+        public QRCoderIDN()
+        {
+            InitializeComponent();
+            ToolTip pictureBoxToolTip = new ToolTip();
+            pictureBoxToolTip.SetToolTip(pictureBox1, "Copy image to clipboard.");
+        }
+
+        private void myQrCoder(string mytext)
+        {
+            // Calculate the desired size for the QR code based on PictureBox size
+            int desiredWidth = pictureBox1.Width;
+            int desiredHeight = pictureBox1.Height;
+            try
+            {
+                using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+                using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(mytext, QRCodeGenerator.ECCLevel.Q))
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    // Get the original QR code image
+                    Bitmap qrCodeImage = qrCode.GetGraphic(10);
+                    // Resize the QR code image to fit within the PictureBox
+                    Bitmap resizedImage = ResizeImage(qrCodeImage, desiredWidth, desiredHeight);
+                    // Display the resized image in the PictureBox
+                    pictureBox1.Image = resizedImage;
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+        }
+
+        private Bitmap ResizeImage(Bitmap originalImage, int newWidth, int newHeight)
+        {
+            Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+
+            using (Graphics g = Graphics.FromImage(resizedImage))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.DrawImage(originalImage, 0, 0, newWidth, newHeight);
+            }
+            return resizedImage;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            myQrCoder(textBox1.Text);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                // Copy image to clipboard
+                Clipboard.SetImage(pictureBox1.Image);
+            }
+            else
+            {
+                MessageBox.Show("No QR code to copy!");
+            }
+        }
+    }
+}
